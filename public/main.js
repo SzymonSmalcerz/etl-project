52bb3a856659f259821f4fc3153d8f019079b09b
zenekshopd2 <smalcerzszymonn@gmail.com>
2019-11-18 21:37:25 +0100

    nothing done here

--- public/main.js
+++ public/main.js
@@ -1,29 +1,27 @@
 $(document).ready(function() {
-    var next = 1;
-    $(".add-more").click(function(e) {
-      console.log("___________");
-        e.preventDefault();
-        document.asd = e;
-        var addto = "#field" + next;
-        var addRemove = "#field" + (next);
-        next = next + 1;
-        var newIn = '<input autocomplete="off" class="input form-control" id="field' + next + '" name="field' + next + '" type="text">';
-        var newInput = $(newIn);
-        var removeBtn = '<button id="remove' + (next - 1) + '" class="btn btn-danger remove-me" >-</button></div><div id="field">';
-        var removeButton = $(removeBtn);
-        $(addto).after(newInput);
-        $(addRemove).after(removeButton);
-        $("#field" + next).attr('data-source',$(addto).attr('data-source'));
-        $("#count").val(next);
-        $('.remove-me').click(function(e){
-            console.log(e);
-            e.preventDefault();
-            var fieldNum = this.id.charAt(this.id.length-1);
-            var fieldID = "#field" + fieldNum;
-            $(this).remove();
-            $(fieldID).remove();
-        });
-    });
+  var next = 1;
+  $(".add-more").click(function(e){
+      e.preventDefault();
+      var addto = "#field" + next;
+      var addRemove = "#field" + (next);
+      next = next + 1;
+      var newIn = '<input autocomplete="off" class="input form-control" id="field' + next + '" name="field' + next + '" type="text">';
+      var newInput = $(newIn);
+      var removeBtn = '<button id="remove' + (next - 1) + '" class="btn btn-danger remove-me" >-</button></div><div id="field">';
+      var removeButton = $(removeBtn);
+      $(addto).after(newInput);
+      $(addRemove).after(removeButton);
+      $("#field" + next).attr('data-source',$(addto).attr('data-source'));
+      $("#count").val(next);
+
+          $('.remove-me').click(function(e){
+              e.preventDefault();
+              var fieldNum = this.id.charAt(this.id.length-1);
+              var fieldID = "#field" + fieldNum;
+              $(this).remove();
+              $(fieldID).remove();
+          });
+  });
 
     $('#submit').click(function(e) {
       $('.input-append').css("display", "none");
@@ -36,7 +34,14 @@ $(document).ready(function() {
       b = new Set(b);
       b = [...b];
       console.log(b);
-      alert("entered keys: " + b);
-      alert("no more for now :C");
+       $.post("/e",{
+         movieKeys : b
+       },(data, status) => {
+         console.log(data);
+         console.log(status);
+       });
+      // alert("entered keys: " + b);
+      // alert("no more for now :C");
+
     });
 });
